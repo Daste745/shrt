0b5b95f478a5c870afb719141fb837eba5b7413f
Stefankar1000 <stefankar1000@gmail.com>
2020-07-28 14:27:37 +0200

    Use sqlite as the project's database

--- shrt.py
+++ shrt.py
@@ -2,20 +2,34 @@ import json
 import shortuuid
 import os
 from flask import Flask, request, redirect, render_template, flash, url_for
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import Form, StringField, validators
-
-
-with open("redirects.json", "r") as f:
-    redirects = json.load(f)
-
-
-def save_redirects() -> None:
-    with open("redirects.json", "w") as file:
-        json.dump(redirects, file, indent=2)
+from datetime import datetime
 
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///shrt.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+db = SQLAlchemy(app)
+
+
+class Redirect(db.Model):
+    __tablename__ = "redirects"
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String)
+    key = db.Column(db.String)
+    created = db.Column(db.DateTime)
+
+    def __init__(self, url: str, key: str):
+        self.url = url
+        self.key = key
+        self.created = datetime.utcnow()
+
+
+# Create db tables if they don't exist
+db.create_all()
 
 
 class RegisterUrlForm(Form):
@@ -33,7 +47,7 @@ class RegisterUrlForm(Form):
             validators.Optional(),
             validators.Length(min=0, max=16, message="Max allowed lenght is 16"),
             validators.Regexp(
-                regex=r"^[a-zA-Z0-9]+$", message="Allowed characters: a-Z, A-Z, 0.9"
+                regex=r"^[a-zA-Z0-9]+$", message="Allowed characters: a-z, A-Z, 0-9"
             ),
         ],
     )
@@ -45,27 +59,27 @@ def index():
 
     if request.method == "POST" and form.validate():
         if not (key := form.key.data):
-            key = shortuuid.random(6)
+            key = shortuuid.random(8)
 
-        if key in redirects.keys():
-            flash((f"is already in use", redirects[key], key), "error")
+        if used := Redirect.query.filter(Redirect.key == key).first():
+            flash((f"is already in use", used.url, key), "error")
         else:
-            url = form.url.data
-            redirects.update({key: url})
-            save_redirects()
+            record = Redirect(form.url.data, key)
+            db.session.add(record)
+            db.session.commit()
 
-            registered_url = url_for("access_url", key=key, _external=True)
-            flash((f"Created short url", registered_url), "success")
+            registered = url_for("access_url", key=record.key, _external=True)
+            flash((f"Created short url", registered), "success")
 
     return render_template("index.html", form=form)
 
 
 @app.route("/<string:key>")
 def access_url(key: str):
-    if key not in redirects.keys():
+    if not (registered := Redirect.query.filter(Redirect.key == key).first()):
         return f"There is no url bound to key '{key}'\n", 404
 
-    return redirect(redirects[key])
+    return redirect(registered.url)
 
 
 if __name__ == "__main__":
